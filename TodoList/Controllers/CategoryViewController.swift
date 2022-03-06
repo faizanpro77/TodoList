af94b2512ec763d7932b8c2a9962badc1c94a305
faizanpro77 <faizyshaikh204@gmail.com>
2022-03-06 17:42:15 +0530

    add new delegate method

--- TodoList/Controllers/CategoryViewController.swift
+++ TodoList/Controllers/CategoryViewController.swift
@@ -16,6 +16,8 @@ class CategoryViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loadCategories()
 
     }
     
@@ -27,15 +29,54 @@ class CategoryViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        //cell.textLabel?.text = categories[indexPath.row].name
+        cell.textLabel?.text = categories[indexPath.row].name
         return cell
     }
     
     //Mark: - Data Manipulation Methods
     
+    func saveCategories() {
+        do {
+        try context.save()
+        } catch {
+            print("Error saving")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func loadCategories(){
+        let request : NSFetchRequest<Category> = Category.fetchRequest()
+        do{
+        categories = try context.fetch(request)
+        } catch {
+            print("Error loading categories \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
     //Mark - Add New Categories
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) { (Action) in
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+            self.categories.append(newCategory)
+            
+            self.saveCategories()
+            
+        }
+        alert.addAction(action)
+        alert.addTextField { (field) in
+            textField = field
+            textField.placeholder = "Add a new category"
+        }
+        present(alert, animated: true, completion: nil)
     }
     
  
